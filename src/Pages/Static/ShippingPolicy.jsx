2b119b1080854dfc7acde46f9662f3fda49c0c61
Sarowar Alam <alamsarowar3@gmail.com>
2024-10-22 15:52:06 +0530

    Update ShippingPolicy.jsx
    dimpi updates

--- src/Pages/Static/ShippingPolicy.jsx
+++ src/Pages/Static/ShippingPolicy.jsx
@@ -3,7 +3,16 @@ import MasterLayout from '../../Layouts/MasterLayout'
 
 const ShippingPolicy = () => {
   return (
-    <MasterLayout  title="Shipping Policy">ShippingPolicy</MasterLayout>
+    <MasterLayout title="Shipping Policy">
+
+      <div className="Shipping-policy-wrapper px-3 py-3 bg-white">
+        <h1>Shipping Policy</h1>
+        <p>All orders will be processed within 48 hours, subject to stock availability.</p>
+        <p>Once processed, the order will be out for delivery within 24 hours.</p>
+        <p>Delivery charges will be free within Guwahati on orders above Rs. 500.</p>
+        <p>For delivery outside Guwahati, delivery charges will be shown on the payment page.</p>
+      </div>
+    </MasterLayout>
   )
 }
 
