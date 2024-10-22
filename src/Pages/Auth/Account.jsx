d4a9ec14400177ac931eb974a5d757c8ef112d87
Sarowar Alam <alamsarowar3@gmail.com>
2024-10-22 16:01:43 +0530

    Update Account.jsx
    update1

--- src/Pages/Auth/Account.jsx
+++ src/Pages/Auth/Account.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import MasterLayout from '../../Layouts/MasterLayout'
+import React from 'react';
+import MasterLayout from '../../Layouts/MasterLayout';
 
 const Account = () => {
+ 
+
   return (
-    <MasterLayout  title="Account">
-      <div className="">
-        Account
+    <MasterLayout title="Account">
+      <div className="login-wrapper d-flex align-items-center justify-content-center text-center">
+        <div className="container">
+          <div className="row justify-content-center">
+            
+          </div>
+        </div>
       </div>
     </MasterLayout>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
