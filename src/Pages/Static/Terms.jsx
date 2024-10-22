c0a4182d2a189300ab360132a6448b6a333dc60b
Sarowar Alam <alamsarowar3@gmail.com>
2024-10-22 15:53:34 +0530

    Update Terms.jsx
    dimpi updates

--- src/Pages/Static/Terms.jsx
+++ src/Pages/Static/Terms.jsx
@@ -3,8 +3,73 @@ import MasterLayout from '../../Layouts/MasterLayout'
 
 const Terms = () => {
   return (
-    <MasterLayout  title="Terms & Conditions">
-        terms
+    <MasterLayout title="Terms & Conditions">
+
+     <div className="Terms-conditions-wrapper px-3 py-3">
+      <h1>Terms & Conditions for Online Payment</h1>
+      
+      <h2>Acceptance of Terms and Conditions</h2>
+      <p>
+        Welcome to Idol Pharma. Idolpharma.in is a Guwahati-based pharma company that prides itself on delivering emergency medicine on time. All information and services displayed on the Idol Pharma website constitute advice on online payment. Your request for the transaction consists of the terms and conditions shared by the pharma. If you do not follow these protocols, kindly avoid using our service.
+      </p>
+      <p>
+        By availing of this facility, you accept a legally binding agreement between you and Idol Pharma.
+      </p>
+
+      <h2>Definitions</h2>
+      <p>
+        "Agreement" means the terms and conditions, including the Privacy Policy, schedules, appendices, and any references that may be added, varied, or replaced from time to time. “User” or "You" refers to the person using the website. "Site" is the online platform provided by Idol Pharma for purchasing drugs. “Product/s” or “Drug” refers to the medications or any products displayed on our website.
+      </p>
+
+      <h2>Modification of Terms</h2>
+      <p>
+        Idol Pharma can modify the terms and conditions without prior notification. You should abide by the terms and conditions when using our website. If you disagree with the terms, you can discontinue the use of the service.
+      </p>
+
+      <h2>Prescribed Medicine Terms and Conditions</h2>
+      <p>
+        To buy drugs from us, you must provide a copy of the prescribed medicine, either physically or by uploading a scanned copy on our website. Our Retail Pharmacy will verify your prescription before sending the medicines.
+      </p>
+
+      <h2>Payment</h2>
+      <p>
+        Payments can be made using your credit card. You are responsible for ensuring that all credit card details provided are correct. Idol Pharma is not liable for any credit card fraud.
+      </p>
+
+      <h2>Product and Service</h2>
+      <p>
+        Our terms also facilitate sales and purchases between third parties and you. Idol Pharma cannot control or be held responsible for transactions between you and third parties.
+      </p>
+
+      <h2>Pricing Information</h2>
+      <p>
+        Pricing information is displayed in Indian Rupees, and all transactions fall under Indian law. If you pay more than the listed price, it is solely your responsibility.
+      </p>
+
+      <h2>Drug Delivery</h2>
+      <p>
+        The Company is not responsible for delays in delivery. By agreeing to the terms, you consent to share your address with delivery personnel.
+      </p>
+
+      <h2>User Obligations</h2>
+      <p>
+        You must not access or request information that may harm the Company’s identity. Any form of abuse, harassment, or threats will not be tolerated. You are also not permitted to delete any data from our website.
+      </p>
+
+      <h2>Notice and Takedown</h2>
+      <p>
+        If you find any offensive or illegal content, please contact us using the number provided on the website.
+      </p>
+
+      <h2>Risks Associated with Company’s Services</h2>
+      <p>
+        You are encouraged to share your medical history before purchasing any medications. We strongly recommend maintaining communication with your physician.
+      </p>
+
+      <p>
+        *These Terms and Conditions may change at the sole discretion of the Company.
+      </p>
+      </div>
     </MasterLayout>
   )
 }
